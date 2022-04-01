648a88f85d6225a2fd8587dfb95a09f3af18ebe6
Sean <seanhcheong@gmail.com>
2022-03-31 19:04:57 -0700

    initial commit

---
+++ src/main/java/com/seanproject/Accountability/AccountabilitySystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.seanproject.Accountability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountabilitySystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountabilitySystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seanproject/Accountability/AccountabilitySystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seanproject.Accountability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountabilitySystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
